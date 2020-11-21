f69c73c0fd9d0919d4ab2b84890baa58f7dc607b
hosermes <mirjalali.hosein@gmail.com>
2020-11-21 19:56:52 +0330

    end of lecture 21

---
+++ src/main/java/hosermes/msscservice/web/model/BeerStyleEnum.java
@@ -0,0 +1,5 @@
+package hosermes.msscservice.web.model;
+
+public enum BeerStyleEnum {
+    LAGER, PILSNER, STOIT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
---
+++ src/test/java/hosermes/msscservice/MsscServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package hosermes.msscservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
